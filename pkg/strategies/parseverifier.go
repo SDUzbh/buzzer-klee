bed5994d04c0f5299831f4c8905663f2b9767f0c
SDUzbh <1124494103@qq.com>
2025-05-07 14:43:33 -0400

    fix

--- pkg/strategies/parseverifier.go
+++ pkg/strategies/parseverifier.go
@@ -23,15 +23,14 @@ package strategies
 import "C"
 
 import (
-	"errors"
 	"fmt"
-	"log"
 
-	. "buzzer/pkg/units/units"
+	"buzzer/pkg/rand"
+	"buzzer/pkg/units/units"
 
-	"buzzer/pkg/ebpf/ebpf"
-	. "buzzer/pkg/strategies/oracle"
+	. "buzzer/pkg/ebpf/ebpf"
 
+	btfpb "buzzer/proto/btf_go_proto"
 	epb "buzzer/proto/ebpf_go_proto"
 	fpb "buzzer/proto/ffi_go_proto"
 	pb "buzzer/proto/program_go_proto"
@@ -54,157 +53,55 @@ type StrategyParseVerifierLog struct {
 	log               string
 }
 
-func (st *StrategyParseVerifierLog) generateAndValidateProgram(e ExecutorInterface, gen *Generator) (*GeneratorResult, error) {
-	for i := 0; i < 100_000; i++ {
-		prog, err := ebpf.New( /*mapSize=*/ 1000 /*minReg=*/, ebpf.R7 /*maxReg=*/, ebpf.R9)
-		gen.logMapFd = prog.LogMap()
-		prog.Instructions = gen.Generate(prog)
-		if err != nil {
-			return nil, err
-		}
-		byteCode := prog.GenerateBytecode()
-		res, err := e.ValidateProgram(byteCode)
-		if err != nil {
-			prog.Cleanup()
-			return nil, err
-		}
+func (pg *StrategyParseVerifierLog) GenerateProgram(ffi *units.FFI) (*pb.Program, error) {
+	pg.programCount += 1
+	fmt.Printf("Generated %d programs, %d were valid               \r", pg.programCount, pg.validProgramCount)
 
-		if res.GetIsValid() {
-			result := &GeneratorResult{
-				Prog:         prog,
-				ProgByteCode: byteCode,
-				ProgFD:       res.GetProgramFd(),
-				VerifierLog:  res.GetVerifierLog(),
-			}
+	// Setup BTF Section
+	btf := &btfpb.Btf{}
+	SetHeaderSection(btf, 0xeb9f, 0x01, 0x0)
+	btf.TypeSection = &btfpb.TypeSection{BtfType: types()}
+	btf.StringSection = &btfpb.StringSection{Str: "buzzer"}
 
-			return result, nil
-		}
-		prog.Cleanup()
+	mapFd := ffi.CreateMapArray(2)
+	ffi.CloseFD(pg.mapFd)
+	pg.mapFd = mapFd
+
+	// Generate Random Function
+	instructionCount := rand.SharedRNG.RandInt() % 1000
+	functionBody, _ := InstructionSequence()
+
+	for i := uint64(0); i < instructionCount; i++ {
+		instruction := RandomAluInstruction() // Generate random ALU instruction
+		functionBody = append(functionBody, instruction)
 	}
-	return nil, errors.New("could not generate a valid program")
-}
 
-// Fuzz implements the main fuzzing logic.
-func (st *StrategyParseVerifierLog) Fuzz(e ExecutorInterface, cm CoverageManager) error {
-	fmt.Printf("running fuzzing strategy %s\n", StrategyName)
-	i := 0
-	for {
-		gen := &Generator{
-			instructionCount: 10,
-			offsetMap:        make(map[int32]int32),
-			sizeMap:          make(map[int32]int32),
-			regMap:           make(map[int32]uint8),
-		}
-		fmt.Printf("Fuzzer run no %d.                               \r", i)
-		i++
-		gr, err := st.generateAndValidateProgram(e, gen)
+	// Function Exit
+	functionBody = append(functionBody, Mov(R0, 0), Exit())
 
-		if err != nil {
-			return err
-		}
-
-		// Build a new execution request.
-		logCount := gen.logCount
-		mapDescription := &fpb.ExecutionRequest_MapDescription{
-			MapFd:   int64(gen.logMapFd),
-			MapSize: uint64(logCount),
-		}
-		executionRequest := &fpb.ExecutionRequest{
-			ProgFd: gr.ProgFD,
-			Maps:   []*fpb.ExecutionRequest_MapDescription{mapDescription},
-		}
-
-		defer func() {
-			C.close_fd(C.int(executionRequest.GetProgFd()))
-			C.close_fd(C.int(mapDescription.GetMapFd()))
-		}()
-
-		programFlaked := true
-
-		var exRes *fpb.ExecutionResult
-		maxAttempts := 1000
-
-		for programFlaked && maxAttempts != 0 {
-			maxAttempts--
-			eR, err := e.RunProgram(executionRequest)
-			if err != nil {
-				return err
-			}
-
-			if !eR.GetDidSucceed() {
-				return fmt.Errorf("execute Program did not succeed")
-			}
-			mapElements := eR.GetMapElements()[0].GetElements()
-			for i := 0; i < len(mapElements); i++ {
-				if mapElements[i] != 0 {
-					programFlaked = false
-					exRes = eR
-					break
-				}
-			}
-		}
-
-		if maxAttempts == 0 {
-			fmt.Println("program flaked")
-			SaveExecutionResults(gr)
-			continue
-		}
-
-		// Program succeeded, let's validate the execution map.
-		regOracle, err := FromVerifierTrace(gr.VerifierLog)
-		if err != nil {
-			log.Fatalf("Failed to build oracle: %v", err)
-		}
-
-		regOracle.Dump()
-
-		mapSize := int32(executionRequest.GetMaps()[0].GetMapSize())
-		mapElements := exRes.GetMapElements()[0].GetElements()
-		for mapIndex := int32(0); mapIndex < mapSize; mapIndex++ {
-			offset := gen.GetProgramOffset(mapIndex)
-			dstReg := gen.GetDestReg(mapIndex)
-			verifierValue, known, err := regOracle.LookupRegValue(offset, dstReg)
-			if err != nil {
-				return err
-			}
-			actualValue := mapElements[mapIndex]
-			if known && verifierValue != actualValue {
-				if err := SaveExecutionResults(gr); err != nil {
-					return err
-				}
-			}
-		}
-
-		C.close_fd(C.int(executionRequest.GetProgFd()))
-		C.close_fd(C.int(mapDescription.GetMapFd()))
-	}
-	return nil
-}
-
-func (pg *StrategyParseVerifierLog) GenerateProgram(ffi *FFI) (*pb.Program, error) {
-
-	insn, err := InstructionSequence(
-		Mov(R0, 0),
-		Exit(),
-	)
+	btfBuffer, err := GetBuffer(btf)
 	if err != nil {
 		return nil, err
 	}
+
 	prog := &pb.Program{
 		Program: &pb.Program_Ebpf{
 			Ebpf: &epb.Program{
 				Functions: []*epb.Functions{
-					{Instructions: insn},
+					{Instructions: functionBody, FuncInfo: &btfpb.FuncInfo{InsnOff: 0, TypeId: 2}},
 				},
+				Btf: btfBuffer,
 			},
-		}}
+		},
+	}
+
 	return prog, nil
 }
 
 // OnVerifyDone process the results from the verifier. Here the strategy
 // can also tell the fuzzer to continue with execution by returning true
 // or start over and generate a new program by returning false.
-func (pg *StrategyParseVerifierLog) OnVerifyDone(ffi *FFI, verificationResult *fpb.ValidationResult) bool {
+func (pg *StrategyParseVerifierLog) OnVerifyDone(ffi *units.FFI, verificationResult *fpb.ValidationResult) bool {
 	fmt.Println(verificationResult.VerifierLog)
 	pg.isFinished = true
 	return true
@@ -212,7 +109,7 @@ func (pg *StrategyParseVerifierLog) OnVerifyDone(ffi *FFI, verificationResult *f
 
 // OnExecuteDone should validate if the program behaved like the
 // verifier expected, if that was not the case it should return false.
-func (pg *StrategyParseVerifierLog) OnExecuteDone(ffi *FFI, executionResult *fpb.ExecutionResult) bool {
+func (pg *StrategyParseVerifierLog) OnExecuteDone(ffi *units.FFI, executionResult *fpb.ExecutionResult) bool {
 	return true
 }
 
@@ -232,4 +129,8 @@ func (pg *StrategyParseVerifierLog) IsFuzzingDone() bool {
 // StrategyName is used for strategy selection via runtime flags.
 func (pg *StrategyParseVerifierLog) Name() string {
 	return "ParseVerifier"
+}
+
+func NewParseVerifierStrategy() *StrategyParseVerifierLog {
+	return &StrategyParseVerifierLog{isFinished: false}
 }
